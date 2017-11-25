90c8c9e84c719f0acb739f89e1586f21da49f237
JRixGeromo <jrixgeromo@gmail.com>
2017-11-25 10:51:08 +0800

    Updates

--- assets/app/calendars/crud/calendar-create.component.ts
+++ assets/app/calendars/crud/calendar-create.component.ts
@@ -3,7 +3,8 @@ import { NgForm } from "@angular/forms";
 
 import { CalendarService } from "./calendar.service";
 import { Calendar } from "./calendar.model";
-import { IMultiSelectOption } from 'angular-2-dropdown-multiselect';
+import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts  } from 'angular-2-dropdown-multiselect';
+//import { IMultiSelectOption, IMultiSelectSettings, IMultiSelectTexts } from '../../../../node_modules/angular-2-dropdown-multiselect';
 
 declare var $:any;
 
@@ -23,8 +24,48 @@ export class CalendarCreateComponent implements OnInit {
     preptime = {hour: 0, minute: 0};
     finishtime = {hour: 0, minute: 0};
 
-    optionsModel: number[];
-    myOptions: IMultiSelectOption[];
+    /////
+
+
+     
+    // Default selection 
+    optionsModel: number[] = [1, 2];
+     
+    // Settings configuration 
+    mySettings: IMultiSelectSettings = {
+        enableSearch: true,
+        checkedStyle: 'fontawesome',
+        buttonClasses: 'btn btn-default btn-block',
+        dynamicTitleMaxItems: 3,
+        displayAllSelectedText: true
+    };
+     
+    // Text configuration 
+    myTexts: IMultiSelectTexts = {
+        checkAll: 'Select all',
+        uncheckAll: 'Unselect all',
+        checked: 'item selected',
+        checkedPlural: 'items selected',
+        searchPlaceholder: 'Find',
+        defaultTitle: 'Select',
+        allSelected: 'All selected',
+    };
+     
+    // Labels / Parents 
+    myOptions: IMultiSelectOption[] = [
+        { id: 1, name: 'Car brands', isLabel: true },
+        { id: 2, name: 'Volvo', parentId: 1 },
+        { id: 3, name: 'Honda', parentId: 1 },
+        { id: 4, name: 'BMW', parentId: 1 },
+        { id: 5, name: 'Colors', isLabel: true },
+        { id: 6, name: 'Blue', parentId: 5 },
+        { id: 7, name: 'Red', parentId: 5 },
+        { id: 8, name: 'White', parentId: 5 }
+    ];
+
+
+    /////
+    //myOptions: IMultiSelectOption[];
     starttime = {hour: 8, minute: 0};
     meridian = true;
   
@@ -79,11 +120,6 @@ export class CalendarCreateComponent implements OnInit {
             (calendar: Calendar) => this.calendar = calendar
         );
 
-
-       this.myOptions = [
-            { id: 1, name: 'Option 1' },
-            { id: 2, name: 'Option 2' },
-        ];
 
     }
      
